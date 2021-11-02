a5a9debae16eb1b13a77e6486c6b68ea557250d1
wszy5 <wikoszyman@wp.pl>
2021-11-02 23:37:18 +0100

    simple calculator in Vue

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,76 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="result">{{this.a}}{{this.b}}</div>
+    <div id="buttons">
+        <button v-for="i in Array.from(Array(10).keys())" :key="i" class="sbtn" @click="passValue(i)">{{i}}</button>
+        <button class="sbtn" @click="clear()">CLEAR</button>
+        <button class="sbtn">=</button>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return{
+      a:null,
+      b:null,
+      result:null
+    }
+  },
+  methods:{
+    passValue(i){
+      if(this.a==null){
+        this.a = i;
+      }
+      else{
+        this.b = i;
+      }
+    },
+    show(){
+      alert(this.a);
+      alert(this.b);
+    },
+    clear(){
+      this.a = null,
+      this.b = null,
+      this.result = null
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+#result{
+  width: 200px;
+  height: 100px;
+  background-color: antiquewhite;
+  padding: 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid black;
+  font-size: 32px;
+}
+
+#buttons{
+  width: 200px;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  
+}
+.sbtn{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 55px;
+  height: 55px;
+  margin: 5px;
 }
 </style>
